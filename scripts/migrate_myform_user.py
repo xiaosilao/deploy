224056f1f2ff04ebeca8fedea823a8ea29433351
root <294035833@qq.com>
2018-09-19 19:18:50 +0800

    zip

--- scripts/migrate_myform_user.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-from assets.models import Project,ProjectUser
-
-for one in Project.objects.filter():
-    for user in one.service_user.all():
-        a =ProjectUser.objects.get_or_create(user=user,myform=one)
